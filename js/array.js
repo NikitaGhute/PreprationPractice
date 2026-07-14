12a58eb1c97a7e7b4c02b17cc9a739112490d7bf
Nikita Ghute <nikitabghute7350@gmail.com>
2026-07-14 23:58:39 +0530

    practised mutable and immutable in array method

--- js/array.js
+++ js/array.js
@@ -23,15 +23,58 @@ console.log("add item at start of array", arr.unshift(9))
 // console.log("splice array item", arr.splice(1,4))
 
 let arrFruit= ['apple', 'banana', 'orange', 'mango', 'kiwi', 'grapes'];
+console.log("original array", arrFruit)
 
 // remove orange 
-console.log("splice add item",arrFruit.splice(2,1)) 
-/* 2 is for index number and 1 is for remove 1 item only*/
+console.log("splice- add item",arrFruit.splice(2,1)) 
+console.log("arrfruit added orange", arrFruit)
+/* 2 is for index number and 1 is count of item to be remove and show mutated array*/
 
-// replace item - 'mango' with 'pineapple'
+// replace item - 'mango' with 'pineapple' 
+/*2 is selected index, 1 is delete count*/
 arrFruit.splice(2,1, 'pineapple')
 console.log("replace", arrFruit)
 
+// add item without deleting single item
+arrFruit.splice(3, 0, "chikoo")
+console.log("item add without delete", arrFruit)
+
+// modify any one item to upper case, (work same as replace)
+arrFruit.splice(3,1, arrFruit[2].toUpperCase())
+// 2 is 2nd index from original array, put selected item at 3rd position to be modify, 1 is modify and delete item
+console.log("toUpperCase", arrFruit)
 
 
-//1.sum of array
+//1.sum of array item
+const numbers = [1, 2, 3, 4, 5, 6];
+const totalsum = numbers.reduce((acc, current) => acc + current, 0);
+console.log("sum of array:", totalsum); 
+
+// sort arrray item
+const sortArr= ["car", "animal", "city", "furniture", "states", "country"]
+console.log(sortArr.sort())
+
+// reverse array- reverses original array, not sorted with alphabets
+console.log("reverse array", arrFruit.reverse())
+
+// fill - fills index from 3-5 with 0
+console.log("fill in arr", numbers.fill(1,3)) 
+// o/p - [1, 2, 3, 1, 1, 1]
+
+// reorder element
+ const letters = ["a", "d", "c", "b", "f", "e"]
+ console.log("reorder array item", letters.sort())
+
+//  IMMUTABLE ARRAY YMETHODS IN JS - returning new array instead of modifying original array also called copying methods
+
+  // replacement of mutable sort() is toSort() immutable
+    const sortTo = [3, 4, 5, 6, 1, 2, 8]
+    console.log("replacement for sort",sortTo.toSorted())
+ 
+  // replacement for reverse is toReversed() in immutable
+    console.log("reverseTo for toReversed()", sortTo.toReversed())
+
+    
+
+
+

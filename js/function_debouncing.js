44a0d1c0f56e0e33e1ff08f6bb214919a52da638
Nikita Ghute <nikitabghute7350@gmail.com>
2026-07-24 00:00:33 +0530

    practised curry method

--- js/function_debouncing.js
+++ js/function_debouncing.js
@@ -21,3 +21,41 @@ const debounceSearch= debounce(search, 2000);
     document
     .getElementById("search")
     .addEventListener("input", debounceSearch);
+
+
+// throttle learning
+
+// const callAPI =()=>{
+//     console.log("call api after click button")
+// } 
+// let lastCall = 0;
+//     const handleClick=()=>{
+//         console.log("throttle click")
+//         let now = Date.now()
+//         console.log(now);
+//         if (now - lastCall >= 1000){
+//             lastCall = now;
+//             callAPI ();
+//         }
+//     }
+ 
+// memoize
+
+
+// currying method- reduce or avoid taking repeatative value or argument
+// 1. create main function 
+    const applyDiscount = (discount) =(price)=> price -discount;
+    // 2. lock 10% fixed discount for every item
+
+    const calcDiscount = (discount) =>{
+        return (price) =>{
+            
+            return price - (price * discount);
+            }
+    }
+
+    const fixedDisc = calcDiscount (10)
+// 3. just add the items price for getting final discount
+    console.log(fixedDisc(30) )
+    // console.log(calcDiscount(70))
+    // console.log(calcDiscount(58))
